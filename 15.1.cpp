444f4c89069f8559cf6c90dfa0d360ed55e15f3d
Mitlond <comp122012016@mail.ru>
2016-12-15 17:56:26 +0300

    kun

---
+++ 15.1.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<climits>
+
+int main() 
+{
+  using namespace std;
+  int a = CHAR_MAX;
+  int b = CHAR_MIN;
+  int c = CHAR_BIT;
+  int x = SCHAR_MAX;
+  int z = SCHAR_MIN;
+  int v = UCHAR_MAX;
+  int q = SHRT_MAX;
+  int l = SHRT_MIN;
+  int g = USHRT_MAX;
+  int n = ULONG_MAX;
+  int j = LONG_MIN;
+  int r = LONG_MAX;
+  int f = UINT_MAX;
+  int p = INT_MIN;
+  int d = INT_MAX;
+  
+  cout<<a<<endl;
+  cout<<b<<endl;
+  cout<<c<<endl;
+  cout<<x<<endl;
+  cout<<z<<endl;
+  cout<<v<<endl;
+  cout<<q<<endl;
+  cout<<l<<endl;
+  cout<<g<<endl;
+  cout<<n<<endl;
+  cout<<j<<endl;
+  cout<<r<<endl;
+  cout<<f<<endl;
+  cout<<p<<endl;
+  cout<<d<<endl;
+    system("pause");
+    return 0;
+}     
---
+++ 15.2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+ int main ()
+ {
+     char alarm = '\b';
+     cout<< alarm << "Don't word!\b\n";
+     cout<< "Altair\"ibn\"La-Ahad!\n";
+     bool Salam ='\t';
+     cout<<Salam<<"Hello word!\t\n";
+     cout<<"GynGame\"Game\"Game Over!\n";
+  system("pause");
+  return 0;   
+     }
